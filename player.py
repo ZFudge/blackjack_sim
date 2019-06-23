62d5ce81bbbd05acdb10474056825906c4b7ea0a
zfudge <zacheryfudge@gmail.com>
2019-06-23 08:46:15 -0600

    person actions

--- player.py
+++ player.py
@@ -1,4 +1,9 @@
-class Person():
+from basic_strategy import Basic_Strategy
+from evaluate import Evaluate
+from hi_lo import Hi_Lo
+
+
+class Person(Evaluate):
 	def __init__(self):
 		self.score = 0
 		self.hand = []
@@ -6,15 +11,50 @@ class Person():
 	def new_hand(self):
 		self.hand = []
 
+	def evaluate(self, card):
+		card = card[0]
+		self.score = self.evaluate_card_value(
+			score=self.score,
+			card=card,
+			)
+
 
 class Dealer(Person):
-	pass
+	def deal(self, shoe, person, num_of_cards=1):
+		for x in range(num_of_cards):
+			card = shoe.draw()
+			person.hand.append(card)
+			person.evaluate(evaluate_card_value)
 
 
 class Player(Person):
 	def __init__(self, bankroll=1000, bet_unit=5, bet_spread=12):
 		Person.__init__(self)
-		self.bankroll = bankroll
-		self.bet_unit = bet_unit
-		self.bet_spread = bet_spread
+		self._bankroll = bankroll
+		self._bet_unit = bet_unit
+		self._bet_spread = bet_spread
 
+
+	@property
+	def bankroll(self):
+		return self._bankroll
+
+	@bankroll.setter
+	def bankroll_increment(self, value):
+		self._bankroll += value
+
+	@property
+	def bet_unit(self):
+		return self._bet_unit
+
+	@bet_unit.setter
+	def bet_unit(self, value):
+		self._bet_unit = value
+
+	@property
+	def bet_spread(self):
+		return self._bet_spread
+
+	@bet_spread.setter
+	def bet_spread(self, value):
+		self._bet_spread = value
