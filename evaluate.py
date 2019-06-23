c23886d2eebdef34a39cba130ab37fa200396e82
zfudge <zacheryfudge@gmail.com>
2019-06-23 08:48:28 -0600

    refactored test

--- evaluate.py
+++ evaluate.py
@@ -8,23 +8,19 @@ class Evaluate():
 		pass
 
 
-	def evaluate_card_value(self, person, card):
+	def evaluate_card_value(self, score, card):
 		card = card[0]
 		card_value = self.get_value(card)
 
-		value_list = [person.score, card_value]
+		value_list = [score, card_value]
 
 		if Evaluate.all_ints(value_list):
-			person.score += card_value
+			return score + card_value
 		elif Evaluate.all_lists(value_list):
-			person.score = [ x + 1 for x in person.score]
+			return [ x + 1 for x in score]
 		else:
-			person.score = Evaluate.pack_list(value_list)
+			return Evaluate.pack_list(value_list)
 
-		# Evaluate.check_bust(person.score)
-
-	# def check_bust():
-		# pass
 
 	def get_value(self, card):
 		if card in Evaluate.high_values:
@@ -51,6 +47,7 @@ class Evaluate():
 		lst = value_list[1]
 		lst = [ x + cval for x in lst]
 		return Evaluate.check_values(lst)
+
 
 	@staticmethod
 	def check_values(lst):
--- test_blackjack.py
+++ test_blackjack.py
@@ -2,10 +2,9 @@ import unittest
 from functools import reduce
 
 from shoe import Shoe
-from evaluate import Evaluate
 from basic_strategy import Basic_Strategy
-from hi_lo import Count
-from player import Player
+from hi_lo import Hi_Lo
+from player import Player, Dealer
 
 
 class BlackjackTest(unittest.TestCase):
@@ -13,42 +12,53 @@ class BlackjackTest(unittest.TestCase):
 	def test_create_deck(self):
 		new_shoe = Shoe()
 		self.assertEqual(len(new_shoe.shoe), 270)
-		print(new_shoe.shoe)
+		# print(new_shoe.shoe)
 
 
 	def test_draw(self):
 		new_shoe = Shoe()
-		for x in range(100):
-			new_shoe.draw()
-		self.assertEqual(len(new_shoe.shoe), 170)
+		for x in range(len(new_shoe.shoe)):
+			card = new_shoe.draw()
+			self.assertEqual(len(card), 2)
+		self.assertEqual(len(new_shoe.shoe), 270)
 
 
 	def test_evaluation(self):
-		eve = Evaluate()
-		new_shoe = Shoe(number_of_decks=1, penetration_percentage=100)
+		new_shoe = Shoe(
+			number_of_decks=1,
+			penetration_percentage=100
+			)
 		player = Player()
 		for card in new_shoe.shoe:
-			eve.evaluate_card_value(
-				person=player,
-				card=card
-				)
+			player.evaluate(card=card)
 		self.assertEqual(player.score, 340)
 
 
 	def test_hi_lo(self):
-		counts = Count()
-		new_shoe = Shoe(number_of_decks=2, penetration_percentage=65)
+		counts = Hi_Lo()
+
+		# new_shoe = Shoe(
+		# 	number_of_decks=2,
+		# 	penetration_percentage=65
+		# 	)
+
+		self.assertEqual(counts.count, 0)
 
 		for x in ['1c','Jd','Qh','Ks','Ah']:
-			counts.count_card(x)
-		self.assertEqual(counts.count, 5)
-		self.assertEqual(new_shoe.deck_count, 2)
+			counts.count = x
+		self.assertEqual(counts.count, -5)
+
+		# self.assertEqual(new_shoe.deck_count, 2)
 
 		for x in ['2d','3h','4s','5h','6c']:
-			counts.count_card(x)
+			counts.count = x
 		self.assertEqual(counts.count, 0)
 
 		for x in ['7d','8h','9s','8h','7c']:
-			counts.count_card(x)
+			counts.count = x
 		self.assertEqual(counts.count, 0)
+
+		for x in ['2d','3h','4s','5h','6c']:
+			counts.count = x
+		self.assertEqual(counts.count, 5)
 
